5c63d2770529b49503769ffed8938bd6db71b55a
Mohammed Hasan <mohammadalghodari@gmail.com>
2025-05-21 02:49:14 +0300

    Addind mainForm and projectForm to the project

---
+++ ManagAndTrackProjectsAndTeams/ManagAndTrackProjectsAndTeams/src/Views/MainForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagAndTrackProjectsAndTeams.src.Views
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PnlSideBar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Guna2Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Guna2Panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Guna2Panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Guna2Panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void MonthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void PbxProfile_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnHome_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are already in home page!");
+        }
+
+        private void BtnTeams_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnProjects_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form will coming soon");
+        }
+
+        private void BtnStatistics_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnTasks_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnSettings_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnLogout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnViewTeamMem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnNewTask_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+
+        private void BtnViewReports_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry! this form coming soon");
+        }
+    }
+}
