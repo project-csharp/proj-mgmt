cbef11fa9b6a9f3da25c53f4364e9573f7e5d8eb
isaacmahyoub <eng.isaacmahyoub@gmail.com>
2025-05-21 18:23:08 +0300

    fix(project): repair .csproj XML structure  - Remove duplicate </EmbeddedResource> at line 87 - Ensure proper nesting of ItemGroup tags - Fixes build errors in Visual Studio

--- ManagAndTrackProjectsAndTeams/ManagAndTrackProjectsAndTeams/Properties/Resources.Designer.cs
+++ ManagAndTrackProjectsAndTeams/ManagAndTrackProjectsAndTeams/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ManagAndTrackProjectsAndTeams.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,14 +57,6 @@ namespace ManagAndTrackProjectsAndTeams.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
